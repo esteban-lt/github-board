3db3af5014d11fb0c71b47bf7cc03a40991dc9ad
esteban-ledezma <ledezmatenaesteban@gmail.com>
2026-02-16 22:01:31 -0600

    Add /auth/login route

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter } from 'react-router';
 import AppLayout from '@layouts/app-layout';
 import DashboardPage from '@dashboard/pages';
 
+const LoginPage = lazy(() => import('@auth/pages/login'));
 const ActivityFeedPage = lazy(() => import('@activity-feed/pages'));
 const RepositoriesPage = lazy(() => import('@repositories/pages'));
 const ConnectRepositoryPage = lazy(() => import('@repositories/connect/pages'));
@@ -12,6 +13,15 @@ const IntegrationsPage = lazy(() => import('@integrations/pages'));
 const SettingPage = lazy(() => import('@settings/pages'));
 
 export const appRouter = createBrowserRouter([
+  {
+    path: '/auth',
+    children: [
+      {
+        path: 'login',
+        element: <LoginPage />
+      },
+    ],
+  },
   {
     path: '/',
     element: <AppLayout />,
