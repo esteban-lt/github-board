c118cf2264993db4bea507acc05a791282207528
esteban-ledezma <ledezmatenaesteban@gmail.com>
2026-03-15 13:50:10 -0600

    Create auth page

--- apps/client/src/router/app-router.tsx
+++ apps/client/src/router/app-router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router';
+import AuthPage from '@/modules/auth/pages/auth-page';
 import AppLayout from '@/layouts/app-layout';
 import DashbaordPage from '@/modules/dashboard/pages/dashboard-page';
 import ActivityFeedPage from '@/modules/activity-feed/pages/activity-feed-page';
 import RepositoriesPage from '@/modules/repositories/pages/repositories-page';
 
 export const appRouter = createBrowserRouter([
+  {
+    path: '/auth',
+    element: <AuthPage />
+  },
   {
     path: '/',
     element: <AppLayout />,
