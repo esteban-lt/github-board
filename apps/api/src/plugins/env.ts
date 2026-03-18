05c307199d4111a6bdb31b2124709fe1e64d9e55
esteban-ledezma <ledezmatenaesteban@gmail.com>
2026-03-17 18:37:59 -0600

    Update enviroment variables

--- apps/api/src/plugins/env.ts
+++ apps/api/src/plugins/env.ts
@@ -3,5 +3,12 @@ import { get } from 'env-var';
 
 export const env = {
   PORT: get('PORT').required().asPortNumber(),
+  NODE_ENV: get('NODE_ENV').required().asEnum(['development', 'production', 'test']) as 'development' | 'production' | 'test',
   DATABASE_URL: get('DATABASE_URL').required().asString(),
+  GITHUB_CLIENT_ID: get('GITHUB_CLIENT_ID').required().asString(),
+  GITHUB_CLIENT_SECRET: get('GITHUB_CLIENT_SECRET').required().asString(), 
+  JWT_SECRET: get('JWT_SECRET').required().asString(),
+  JWT_EXPIRES_IN: get('JWT_EXPIRES_IN').required().asString(),
+  FRONTEND_URL: get('FRONTEND_URL').required().asString(),
+  ENCRYPTION_KEY: get('ENCRYPTION_KEY').required().asString(),
 }
