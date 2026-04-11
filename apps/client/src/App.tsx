3b7d803ade24f1ec3a3ec6e6531eed1157d321b3
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-11 17:54:47 -0600

    Add Tanstack QueryClient

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,13 +1,20 @@
 import { RouterProvider } from 'react-router/dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AuthProvider } from './modules/auth/context/auth-context';
 import { appRouter } from './router/app-router';
+
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
-    <AuthProvider>
-      <RouterProvider router={appRouter} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={appRouter} />
+        <ReactQueryDevtools initialIsOpen={false}/>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
 
