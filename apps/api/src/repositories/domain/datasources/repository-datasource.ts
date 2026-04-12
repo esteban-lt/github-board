289351bee2b8a665fb36acb2f6bdb449d3e3a382
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-12 14:43:33 -0600

    feat: add domain datasource and repository interfaces

---
+++ apps/api/src/repositories/domain/datasources/repository-datasource.ts
@@ -0,0 +1,10 @@
+import type { Repository } from '../entities/repository';
+import type { GitHubRepositoryData } from '../interfaces/github-repository-data';
+
+export interface RepositoryDatasource {
+  connect(githubRepoId: number, workspaceId: string, gitHubRepositoryData: GitHubRepositoryData): Promise<Repository>;
+  getAll(workspaceId: string): Promise<Repository[]>;
+  getById(id: string): Promise<Repository>;
+  setStatus(id: string, isActive: boolean): Promise<void>;
+  synchronize(id: string): Promise<Repository>;
+}
