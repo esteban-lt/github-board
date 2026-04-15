795210ff3edaac51a19f5b677ebf1ee1f2fa86e8
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-14 23:39:33 -0600

    update Repository entity

--- apps/api/src/repositories/domain/entities/repository.ts
+++ apps/api/src/repositories/domain/entities/repository.ts
@@ -1,3 +1,23 @@
+interface Options {
+  id: string;
+  workspaceId: string;
+  githubRepoId: number;
+  name: string;
+  fullName: string;
+  description: string;
+  htmlUrl: string;
+  language: string;
+  stars: number;
+  forks: number;
+  openIssues: number;
+  openPullRequests: number;
+  defaultBranch: string;
+  isConnected: boolean;
+  isActive: boolean;
+  isPrivate: boolean;
+  lastSyncedAt: Date;
+}
+
 export class Repository {
   public readonly id: string;
   public readonly workspaceId: string;
@@ -6,24 +26,18 @@ export class Repository {
   public readonly fullName: string;
   public readonly description: string;
   public readonly htmlUrl: string;
+  public readonly language: string;
+  public readonly stars: number;
+  public readonly forks: number;
+  public readonly openIssues: number;
+  public readonly openPullRequests: number;
   public readonly defaultBranch: string;
+  public readonly isConnected: boolean;
   public readonly isActive: boolean;
   public readonly isPrivate: boolean;
   public readonly lastSyncedAt: Date;
 
-  constructor(options: {
-    id: string;
-    workspaceId: string;
-    githubRepoId: number;
-    name: string;
-    fullName: string;
-    description: string;
-    htmlUrl: string;
-    defaultBranch: string;
-    isActive: boolean;
-    isPrivate: boolean;
-    lastSyncedAt: Date;
-  }) {
+  constructor(options: Options) {
     this.id = options.id;
     this.workspaceId = options.workspaceId;
     this.githubRepoId = options.githubRepoId;
@@ -31,7 +45,13 @@ export class Repository {
     this.fullName = options.fullName;
     this.description = options.description;
     this.htmlUrl = options.htmlUrl;
+    this.language = options.language;
+    this.stars = options.stars;
+    this.forks = options.forks;
+    this.openIssues = options.openIssues;
+    this.openPullRequests = options.openPullRequests;
     this.defaultBranch = options.defaultBranch;
+    this.isConnected = options.isConnected;
     this.isActive = options.isActive;
     this.isPrivate = options.isPrivate;
     this.lastSyncedAt = options.lastSyncedAt;
