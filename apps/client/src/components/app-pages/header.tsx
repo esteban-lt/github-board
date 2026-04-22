c9e16ae7c5899ed8d04a76f8607f617a3e989591
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-22 17:31:08 -0600

    style: change h1 font-medium to font-bold

--- apps/client/src/components/app-pages/header.tsx
+++ apps/client/src/components/app-pages/header.tsx
@@ -10,7 +10,7 @@ export const Header = ({ title, description, actions }: Props) => {
   return (
     <div className="flex justify-between items-start">
       <div>
-        <h1 className="text-xl font-medium">{title}</h1>
+        <h1 className="text-xl font-bold">{title}</h1>
         <p className="text-sm">{description}</p>
       </div>
 
