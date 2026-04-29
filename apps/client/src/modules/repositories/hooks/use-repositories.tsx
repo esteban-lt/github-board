4d17ca956161a60e7e8f19d99d7272948fe4190b
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-29 17:40:58 -0600

    add params interfaces

--- apps/client/src/modules/repositories/hooks/use-repositories.tsx
+++ apps/client/src/modules/repositories/hooks/use-repositories.tsx
@@ -1,11 +1,19 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query";
 import { getRepositoriesAction } from "../actions/get-repositories-action";
 
-export const useRepositories = () => {
-  
+interface UseRepositoriesParams {
+  page?: number;
+  limit?: number;
+  search?: string;
+  language?: string;
+  sort?: string;
+  order?: string;
+}
+
+export const useRepositories = (params: UseRepositoriesParams = {}) => {
   return useQuery({
-    queryKey: ['repositories'],
-    queryFn: getRepositoriesAction,
+    queryKey: ['repositories', params],
+    queryFn: () => getRepositoriesAction(params),
     staleTime: 1000 * 60 * 5,
   });
 }
