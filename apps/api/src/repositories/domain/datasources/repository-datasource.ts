47975d800dfe42bcfaea6ddc217a4b216279f2bb
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-29 17:35:33 -0600

    use GetRepositoriesParams and PaginatedRepositores

--- apps/api/src/repositories/domain/datasources/repository-datasource.ts
+++ apps/api/src/repositories/domain/datasources/repository-datasource.ts
@@ -1,10 +1,11 @@
 import type { Repository } from '../entities/repository';
 import type { GitHubRepository } from '../entities/github-repository';
+import type { GetRepositoriesParams, PaginatedRepositories } from '../types/repository-params';
 
 export interface RepositoryDatasource {
   connect(githubRepoId: number, workspaceId: string, gitHubRepository: GitHubRepository): Promise<Repository>;
   disconnect(id: string): Promise<void>;
-  getAll(workspaceId: string): Promise<Repository[]>;
+  getAll(workspaceId: string, params: GetRepositoriesParams): Promise<PaginatedRepositories>;
   getById(id: string): Promise<Repository>;
   getByGithubRepoId(githubRepoId: number): Promise<Repository | null>;
   setStatus(id: string, isActive: boolean): Promise<void>;
