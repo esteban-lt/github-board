220178a55e73c0b894c5ad3b0b33461a1f88818c
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-29 17:43:49 -0600

    update filters to server side

--- apps/client/src/modules/repositories/components/repository-filters.tsx
+++ apps/client/src/modules/repositories/components/repository-filters.tsx
@@ -1,4 +1,3 @@
-import type { Repository } from "../../../interfaces/repository";
 import { LanguageFilter } from "./filters/languaje-filter";
 import { RepositorySearch } from "./filters/repository-search";
 import { RepositoryStatusTabs, type RepositoryStatus } from "./filters/repository-status-tabs";
@@ -6,7 +5,8 @@ import { SortFilter, type RepositorySort } from "./filters/sort-filter";
 import { ViewToggle, type RepositoryView } from "./filters/view-toggle";
 
 interface Props {
-  repositories: Repository[];
+  languages: string[];
+  counts?: Partial<Record<RepositoryStatus, number>>;
   search: string;
   onSearchChange: (value: string) => void;
   status: RepositoryStatus;
@@ -20,26 +20,14 @@ interface Props {
 }
 
 export const RepositoryFilters = ({
-  repositories,
+  languages,
+  counts,
   search, onSearchChange,
   status, onStatusChange,
   language, onLanguageChange,
   sort, onSortChange,
   view, onViewChange,
 }: Props) => {
-
-  const languages = [...new Set(
-    repositories.map((r) => r.language).filter(Boolean)
-  )];
-
-  const counts: Partial<Record<RepositoryStatus, number>> = {
-    all: repositories.length,
-    active: repositories.filter((r) => r.isActive).length,
-    inactive: repositories.filter((r) => !r.isActive).length,
-    public: repositories.filter((r) => !r.isPrivate).length,
-    private: repositories.filter((r) => r.isPrivate).length,
-  };
-
   return (
     <div className="flex items-center gap-2">
       <RepositorySearch value={search} onChange={onSearchChange} />
