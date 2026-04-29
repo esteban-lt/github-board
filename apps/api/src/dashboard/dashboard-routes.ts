501038999886f07e6840cd50df125a319298d173
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-28 22:59:17 -0600

    feat: add commit-activity endpoint

--- apps/api/src/dashboard/dashboard-routes.ts
+++ apps/api/src/dashboard/dashboard-routes.ts
@@ -2,17 +2,21 @@ import { Router } from 'express';
 import { DashboardController } from './dashboard-controller';
 import { DashboardService } from './dashboard-service';
 import { DashboardRepository } from './dashboard-repository';
+import { AuthMiddleware } from '@middlewares/auth-middleware';
+import { EventRepository } from '@events/event-repository';
 
 export class DashboardRoutes {
 
   public static get routes() {
     const router = Router();
-    const dashboardRepository = new DashboardRepository();
+    const eventRepository = new EventRepository();
+    const dashboardRepository = new DashboardRepository(eventRepository);
     const dashboardService = new DashboardService(dashboardRepository);
     const dashboardController = new DashboardController(dashboardService);
 
-    router.get('/stats', dashboardController.getStats);
-    router.get('/top-repositories', dashboardController.getTopRepositories);
+    router.get('/stats', AuthMiddleware.requireAuth, dashboardController.getStats);
+    router.get('/top-repositories', AuthMiddleware.requireAuth, dashboardController.getTopRepositories);
+    router.get('/commit-activity', AuthMiddleware.requireAuth, dashboardController.getCommitActivity)
     // router.use('/api/dashboard/recent-activity');
 
     return router;
