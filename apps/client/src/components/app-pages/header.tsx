689ab277a981fc5cefcc8fd935b65b43b8659d4e
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-30 18:26:42 -0600

    feat: add titleSlot prop

--- apps/client/src/components/app-pages/header.tsx
+++ apps/client/src/components/app-pages/header.tsx
@@ -2,19 +2,23 @@ import type React from "react";
 
 interface Props {
   title: string;
+  titleSlot?: React.ReactElement;
   description?: string;
   actions?: React.ReactElement;
 }
 
-export const Header = ({ title, description, actions }: Props) => {
+export const Header = ({ title, titleSlot, description, actions }: Props) => {
   return (
     <div className="flex justify-between items-start">
       <div>
-        <h1 className="text-xl font-bold">{title}</h1>
+        <div className="flex items-center gap-2">
+          <h1 className="text-xl font-bold">{title}</h1>
+          {titleSlot}
+        </div>
         <p className="text-sm">{description}</p>
       </div>
 
-      <div>
+      <div className="flex gap-2">
         {actions}
       </div>
     </div>
