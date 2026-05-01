4c283bab7859d95b9026dc92fd4f87c23ae696f5
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-30 18:26:25 -0600

    add Suspense from react

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,18 +1,20 @@
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router/dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'sonner';
 import { AuthProvider } from './modules/auth/context/auth-context';
-import { appRouter } from './router/app-router';
+import { router } from './router';
 
 const queryClient = new QueryClient();
 
 function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <RouterProvider router={appRouter} />
+        <Suspense fallback={null}>
+          <RouterProvider router={router} />
+        </Suspense>
         <ReactQueryDevtools initialIsOpen={false}/>
         <Toaster />
       </AuthProvider>
