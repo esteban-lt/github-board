a6dadb0185ba4c111e304051add64ffb83b44f5e
esteban-lt <ledezmatenaesteban@gmail.com>
2026-05-02 14:54:19 -0600

    add getByIdMethod

--- apps/api/src/users/user-repository.ts
+++ apps/api/src/users/user-repository.ts
@@ -3,6 +3,23 @@ import { Encryption } from '@plugins/encryption';
 
 export class UserRepository {
 
+  static getById = async (id: string) => {
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        email: true,
+        avatarUrl: true,
+      }
+    });
+
+    if(!user) throw new Error('User not found');
+
+    return user;
+  }
+
   static getAccessToken = async (userId: string) => {
     const user = await prisma.user.findUnique({
       where: {
